a0004898557a933e65ca405e42754c23937ee07e
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-27 12:20:31 -0500

    Create the App store.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,11 +3,9 @@ import { composeWithDevTools } from "@redux-devtools/extension";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import toursReducer from './tours/toursReducer';
-import bookingsReducer from './bookings/bookingsReducer';
 
 const rootReducer = combineReducers({
   toursReducer,
-  bookingsReducer,
 });
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
