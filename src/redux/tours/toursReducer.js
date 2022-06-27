75074c574ca5da0415fc933dd614c2285d63156e
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-27 12:20:50 -0500

    Create the tours reducer.

---
+++ src/redux/tours/toursReducer.js
@@ -0,0 +1,25 @@
+const baseURL = 'https://localhost:3001/api/v1';
+
+const GET_TOURS_FROM_API = 'GET_TOURS_FROM_API';
+
+const initialState = [];
+
+const getToursAPI = () => async(dispatch) => {
+  await fetch(`${baseURL}/tours`)
+  .then((response) => response.json())
+  .then((data) => {
+    dispatch({
+      type: GET_TOURS_FROM_API,
+      payload: data,
+    });
+  })
+};
+
+const toursReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_TOURS_FROM_API:
+      return action.payload;
+    default:
+      return state;
+  }
+};
